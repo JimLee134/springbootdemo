894b4c7d65d675e33a20dbfddd67a06be1ede5f8
Administrator <1433849049@qq.com>
2019-10-12 19:15:58 +0800

    test

---
+++ src/main/java/com/wj/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,16 @@
+package com.wj.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.servlet.Servlet;
+import java.awt.*;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+}
